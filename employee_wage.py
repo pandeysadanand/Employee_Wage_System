c21a189bcc60340a4cf9331b91f1aedbfbaa4dea
sadanand pandey <pandeysadanad75@gmail.com>
2022-06-03 15:10:57 +0530

    Added: checking employee present or absent

--- employee_wage.py
+++ employee_wage.py
@@ -1,2 +1,24 @@
-class Empployee:
-    
+"""
+    @Author: SADANAND PANDEY
+    @Date: 2022-06-02 19:00:00
+    @Last Modified by: SADANAND PANDEY
+    @Last Modified time: 2022-06-02 19:10:00
+    @Title : Check Employee is Present or Absent
+
+"""
+import random
+
+
+class Employee:
+    @staticmethod
+    def check_attendance():
+        is_present = (random.randint(0, 1))
+        if is_present == 1:
+            print("Employee is present")
+        else:
+            print("Employee is Absent")
+
+
+if __name__ == '__main__':
+    print("Welcome To Employee Wage Management System !!\n")
+    Employee.check_attendance()
